c5a55948e807f9c6814f1100df6ae172f5b89557
nambh911 <nambh911@gmail.com>
2024-06-25 20:57:37 -0700

    update todoproject/settings.py

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = 'your-secret-key'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
